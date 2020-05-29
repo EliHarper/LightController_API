ba135bf3732a89701f3d00583ca7278d6376fc12
EliHarper <eli.harper1995@gmail.com>
2020-05-29 08:13:54 -0400

    Working on the flask post request

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from actions import apply
 from bson import json_util, BSON
 from flask import Flask
+from flask import request
 from flask_cors import CORS
 from kafka import KafkaProducer
 from pymongo import MongoClient
@@ -8,6 +9,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 app.config["DEBUG"] = True
 CORS(app)
+
 mongoClient = MongoClient()
 db = mongoClient.lightdb
 scenes = db.scenes
@@ -18,10 +20,16 @@ def home():
     docs = scenes.find()
     return json_util.dumps(docs)
 
-@app.route('/scene/create', methods=['POST'])
+
+@app.route('/scene/create', methods=['POST', 'OPTIONS'])
 def createScene():
-    jsonDoc = request.get_json()
-    print(jsonDoc)
+    # import pdb; pdb.set_trace()
+    jsonDoc = request.json.get('scene')
+    print('jsonDoc: %', jsonDoc)
+    bsonDoc = BSON.encode(jsonDoc)
+    print('bsonDoc: %', bsonDoc)
+    newId = scenes.insert_one(bsonDoc).inserted_id
+    return newId
     # scene_id = scenes.insert_one()
 
 
@@ -30,5 +38,6 @@ def applyScene(scene_id):
     toApply = scenes.find_one({"_id": scene_id})
     toApply = BSON.decode(toApply)
     apply(toApply)
+
 
 app.run(host='0.0.0.0')
